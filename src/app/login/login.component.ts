c2325b96945bafed596306ab7498b1b878b7ee42
Emmanuel Rivera <129634455+emmannem@users.noreply.github.com>
2023-11-13 10:16:30 -0600

    Finish Login #1 (#8)
    * Modified Login #1
    
    * inicio de sesion implementada
    
    * Modified Login #1
    
    * Update Login branch #1
    
    ---------
    
    Co-authored-by: obed <obedhipolito@gmail.com>

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,77 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ApiService } from '../services/api.service';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import { corr } from 'mathjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  correoPlaceholder: string = 'Correo Electrónico';
+  contrasenaPlaceholder: string = 'Contraseña';
+  loginForm!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private apiService: ApiService,
+    private authService: AuthService,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) {
+    this.loginForm = this.fb.group({
+      correo: [
+        '',
+        [Validators.required, Validators.pattern(/^.+@estudiantes.uv.mx$/)],
+      ],
+      contraseña: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      this.snackBar.open(
+        'Por favor, completa todos los campos correctamente.',
+        'Cerrar',
+        {
+          duration: 3000,
+        }
+      );
+      return;
+    } else {
+      const correo = this.loginForm.get('correo')?.value;
+      const contraseña = this.loginForm.get('contraseña')?.value;
+
+      const credentials = [correo, contraseña];
+
+      this.apiService.iniciarSesion(credentials).subscribe(
+        (response: any) => {
+          const token = response.token;
+          console.log('Token recibido:', token);
+
+          this.authService.setToken(token);
+
+          this.snackBar.open('Inicio de sesión exitoso.', 'Cerrar', {
+            duration: 3000,
+          });
+          this.router.navigate(['/inicio']);
+        },
+        (error: any) => {
+          console.log('credenciales', credentials);
+          console.error('Error de inicio de sesión:', error);
+          this.snackBar.open(
+            'Error de inicio de sesión: ' + error.error.message,
+            'Cerrar',
+            {
+              duration: 3000,
+            }
+          );
+        }
+      );
+    }
+  }
+}
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private apiUrl = 'http://localhost:3000/api/v1/estudiantes';
+
+  constructor(private http: HttpClient) {}
+
+  obtenerToken(correo: string): Observable<any> {
+    const valor = { correo };
+    return this.http.post(`${this.apiUrl}/generarToken`, valor);
+  }
+
+  iniciarSesion(credentials: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/IniciarSesion`, credentials);
+  }
+}
